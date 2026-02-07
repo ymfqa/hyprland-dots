19f8445a1e11ec710402936f047dfc9404d6cfc2
hamster-sama <ymfqa114514@outlook.com>
2026-02-07 21:27:42 +0800

    feat!:修改了很多·
    具体直接看修改部分

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
-	set -gx PATH $PATH ~/go/bin
-	set -gx ANDROID_HOME $HOME/Android/Sdk
-	set -gx PATH $ANDROID_HOME/cmdline-tools/latest/bin $PATH
-	set -gx PATH $ANDROID_HOME/platform-tools $PATH
-	set -gx PATH $ANDROID_HOME/tools/bin $PATH
-	set -gx PATH $ANDROID_HOME/emulator $PATH
+	# source /usr/share/fish/vendor_completions.d/yay.fish
+	# set -gx PATH $PATH ~/go/bin
+	# set -gx ANDROID_HOME $HOME/Android/Sdk
+	# set -gx PATH $ANDROID_HOME/cmdline-tools/latest/bin $PATH
+	# set -gx PATH $ANDROID_HOME/platform-tools $PATH
+	# set -gx PATH $ANDROID_HOME/tools/bin $PATH
+	# set -gx PATH $ANDROID_HOME/emulator $PATH
 end
+
+# Added by Qoder CLI installer
+set -gx PATH $PATH /home/hamster/.local/bin
